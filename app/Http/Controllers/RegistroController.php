bb9164f4442d5a5eaa0b1d495906620aabad2296
luizgustavo1004 <senailuizgustavo@gmail.com>
2025-09-30 13:26:21 -0300

    terminado o controler de registro para poder fazer mesclagem com o IOT

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -3,16 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Registro;
+use App\Models\Sensor;
 use Illuminate\Http\Request;
 
 class RegistroController
 {
+
     public function store(Request $request){
+
+        $sensor = Sensor::where('codigo', '=', $request->codigo)->first();
+        if($sensor == null){
+            return response()->json([
+                'status' => false,
+                'message' => 'codigo nao encontrado'
+            ]);
+        } 
+        
+        
         $registro = Registro::create([
-            'sensor_id' => $request->sensor_id,
+            'sensor_id' => $sensor->id,
             'valor' => $request->valor,
             'unidade' => $request->unidade,
-            'data_hora' => $request->data_hora
+            'data_hora' => date('Y/m/d H:i:s')
         ]);
             return response()->json([
                 'status' => true,
